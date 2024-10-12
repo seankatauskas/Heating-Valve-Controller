3a3dfed141258925ad6279610dff0a5cd9101b5b
seankatauskas <seankatauskas@outlook.com>
2024-10-11 20:17:12 -0500

    Initial Commit: Heating Valve Controller working based off light example

---
+++ README.md
@@ -0,0 +1,74 @@
+# Heating Valve Controller
+
+This project demonstrates a Heating Valve Controller utilizing the Matter protocol and the ESP Matter data model. Written in C++ with C APIs from the ESP32 platform, it highlights my strong skills in embedded systems and end-to-end IoT development.
+
+### Why This Project?
+
+My apartment has a steam radiator with a manual valve requiring me to constantly turn it on and off to maintain an comfortable indoor temperature. After exploring available solutions, I found that most options required significant modifications to the existing heating system and weren't easy to install. To address this, I developed a solution that regulates heating in real-time, has a universal attachment, and works with all smart home platforms.
+
+## Project Overview
+
+### Features
+
+- Matter Protocol Integration: The controller is compatible with any Matter supported device.
+- Servo Motor Control: The servo is halted with feedback from a current sensor when hitting physical resistance.
+- Power Optimization: The code implements low power optimization for the ESP board and disconnects the motor's power source when not in use.
+- 3D Prining: The design has an adjustable mounting bracket and features a motor shaft with an internal sliding rail to accommodate the vertical movement of rotating the handle.
+
+### 1. Hardware Components
+
+-   ESP32-C6-DevKitC-1-N8 Development Board
+-   HiTEC HSR-2648CR Continuous Servo
+-   Tiny Breadboard
+-   Assorted Jumper Wires
+-   INA219 High Side DC Current Sensor Breakout
+-   LM3671 3.3V Buck Converter Breakout
+-   **2** 2x2 AA Battery Holder
+-   **7** AA Batteries
+-   3D Printed Custom Components
+
+### 2. Installation & Building
+   1. Hardware: Connect the components as follows in the connection diagram below.
+   2. Software: Set up the ESP-IDF and ESP Matter environment and flash the firmware.
+   3. Run the controller: Configure the controller with your Matter enabled device and set automations for the on/off switch to certain temperatures.
+
+   For detailed instruction, visit the [ESP Matter Documentation](https://docs.espressif.com/projects/esp-matter/en/latest/esp32/developing.html). This project is based on the [light example](https://github.com/espressif/esp-matter/blob/42b6412/examples/light/README.md) in the esp-matter documentation.
+
+### 3. Connection Diagram
+
+```
+               Power  Power
+               (4.5V) (6V)    
+                 |     |
++-------------+  |     |  +-------------------+         +---------------------------+ 
+| LM3671 Buck |  |     |  |  INA219           |      <|-|  HiTEC HSR-2648CR         |
+| Converter   |  |     |  |  Current Sensor   |         |  Continuous Servo         |
+|             |  |     |  |                   |     +-->|                           |
+|         Vin +<-+     +->+ VIN+          GND +-|>  |   |                           |
+|             |           |                   |     |   +----|   |------------------+ 
+|         GND +-|>  +---->+ VCC          VIN- +-----+        |   |     |           
+|             |     |     |                   |             |||||||    |
+|        3.3v +-----+   +-+ SCL           SCA |-+           -------    |
+|             |     |   | |                   | |                      |
++-------------+     |   | +-------------------+ |                      |
+                    |   +----+         +--------+                      |
+                    v        v         v                               |
+         +----------+--------+---------+--------+                      |
+         |         3.3V    GPIO-3   GPIO-2      |                      |
+         |                               GPIO-0 +<---------------------+
+         |                                      |
+         |                                  GND +-|>
+         |                ESP32-C6              |
+         +--------------------------------------+
+
+```
+
+## Blog & Deep Dive
+
+Explore the technical aspects of the project in my blog:
+
+- [Designing a Matter Heating Valve Controller: Challenges and Solutions](blog-link-section1)
+- [Heating Valve Controller: Product Design](blog-link-section2)
+- [Heating Valve Controller: Power Optimization](blog-link-section3)
+
+These posts provide insights into design choices, challenges faced, and optimizations.
---
+++ main/app_driver.cpp
@@ -0,0 +1,279 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <esp_log.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <esp_matter.h>
+#include "bsp/esp-bsp.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/mcpwm_prelude.h"
+
+#include <app_priv.h>
+
+// Continuous servo control parameters
+#define SERVO_STOP_PULSEWIDTH_US 1500  // Pulse width to stop the servo
+#define SERVO_MAX_PULSEWIDTH_US 2000  // Maximum pulse width for full speed CW
+#define SERVO_MIN_PULSEWIDTH_US 1000  // Minimum pulse width for full speed CCW
+
+#define SERVO_PULSE_GPIO             0        // GPIO connects to the PWM signal line
+#define SERVO_TIMEBASE_RESOLUTION_HZ 1000000  // 1MHz, 1us per tick
+#define SERVO_TIMEBASE_PERIOD        20000    // 20000 ticks, 20ms (50Hz)
+
+using namespace chip::app::Clusters;
+using namespace esp_matter;
+
+static const char *TAG = "app_driver";
+extern uint16_t light_endpoint_id;
+
+mcpwm_timer_handle_t timer = NULL;
+mcpwm_oper_handle_t oper = NULL;
+mcpwm_cmpr_handle_t comparator = NULL;
+mcpwm_gen_handle_t generator = NULL;
+
+
+/* Do any conversions/remapping for the actual value here */
+static esp_err_t app_driver_light_set_power(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+#if CONFIG_BSP_LEDS_NUM > 0
+    esp_err_t err = ESP_OK;
+    ESP_LOGI(TAG, "Enable and start timer");
+    ESP_ERROR_CHECK(mcpwm_timer_enable(timer));
+    ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_START_NO_STOP));
+    if (val->val.b) {
+        err = led_indicator_start(handle, BSP_LED_ON);
+        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, SERVO_MAX_PULSEWIDTH_US));
+        vTaskDelay(pdMS_TO_TICKS(3000));
+        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, SERVO_STOP_PULSEWIDTH_US));
+        vTaskDelay(pdMS_TO_TICKS(3000));
+    } else {
+        err = led_indicator_start(handle, BSP_LED_OFF);
+        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, SERVO_MIN_PULSEWIDTH_US));
+        vTaskDelay(pdMS_TO_TICKS(3000));
+        ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, SERVO_STOP_PULSEWIDTH_US));
+        vTaskDelay(pdMS_TO_TICKS(3000));
+    }
+    ESP_LOGI(TAG, "Stopping and disabiling timer");
+    ESP_ERROR_CHECK(mcpwm_timer_start_stop(timer, MCPWM_TIMER_STOP_EMPTY));
+    ESP_ERROR_CHECK(mcpwm_timer_disable(timer));
+    return err;
+#else
+    ESP_LOGI(TAG, "LED set power: %d", val->val.b);
+    return ESP_OK;
+#endif
+}
+
+static esp_err_t app_driver_light_set_brightness(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+    int value = REMAP_TO_RANGE(val->val.u8, MATTER_BRIGHTNESS, STANDARD_BRIGHTNESS);
+#if CONFIG_BSP_LEDS_NUM > 0
+    return led_indicator_set_brightness(handle, value);
+#else
+    ESP_LOGI(TAG, "LED set brightness: %d", value);
+    return ESP_OK;
+#endif
+}
+
+static esp_err_t app_driver_light_set_hue(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+    int value = REMAP_TO_RANGE(val->val.u8, MATTER_HUE, STANDARD_HUE);
+#if CONFIG_BSP_LEDS_NUM > 0
+    led_indicator_ihsv_t hsv;
+    hsv.value = led_indicator_get_hsv(handle);
+    hsv.h = value;
+    return led_indicator_set_hsv(handle, hsv.value);
+#else
+    ESP_LOGI(TAG, "LED set hue: %d", value);
+    return ESP_OK;
+#endif
+}
+
+static esp_err_t app_driver_light_set_saturation(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+    int value = REMAP_TO_RANGE(val->val.u8, MATTER_SATURATION, STANDARD_SATURATION);
+#if CONFIG_BSP_LEDS_NUM > 0
+    led_indicator_ihsv_t hsv;
+    hsv.value = led_indicator_get_hsv(handle);
+    hsv.s = value;
+    return led_indicator_set_hsv(handle, hsv.value);
+#else
+    ESP_LOGI(TAG, "LED set saturation: %d", value);
+    return ESP_OK;
+#endif
+}
+
+static esp_err_t app_driver_light_set_temperature(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
+{
+    uint32_t value = REMAP_TO_RANGE_INVERSE(val->val.u16, STANDARD_TEMPERATURE_FACTOR);
+#if CONFIG_BSP_LEDS_NUM > 0
+    return led_indicator_set_color_temperature(handle, value);
+#else
+    ESP_LOGI(TAG, "LED set temperature: %ld", value);
+    return ESP_OK;
+#endif
+}
+
+static void app_driver_button_toggle_cb(void *arg, void *data)
+{
+    ESP_LOGI(TAG, "Toggle button pressed");
+    uint16_t endpoint_id = light_endpoint_id;
+    uint32_t cluster_id = OnOff::Id;
+    uint32_t attribute_id = OnOff::Attributes::OnOff::Id;
+
+    attribute_t *attribute = attribute::get(endpoint_id, cluster_id, attribute_id);
+
+    esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+    attribute::get_val(attribute, &val);
+    val.val.b = !val.val.b;
+    attribute::update(endpoint_id, cluster_id, attribute_id, &val);
+}
+
+esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_t endpoint_id, uint32_t cluster_id,
+                                      uint32_t attribute_id, esp_matter_attr_val_t *val)
+{
+    esp_err_t err = ESP_OK;
+    if (endpoint_id == light_endpoint_id) {
+        led_indicator_handle_t handle = (led_indicator_handle_t)driver_handle;
+        if (cluster_id == OnOff::Id) {
+            if (attribute_id == OnOff::Attributes::OnOff::Id) {
+                err = app_driver_light_set_power(handle, val);
+            }
+        } else if (cluster_id == LevelControl::Id) {
+            if (attribute_id == LevelControl::Attributes::CurrentLevel::Id) {
+                err = app_driver_light_set_brightness(handle, val);
+            }
+        } else if (cluster_id == ColorControl::Id) {
+            if (attribute_id == ColorControl::Attributes::CurrentHue::Id) {
+                err = app_driver_light_set_hue(handle, val);
+            } else if (attribute_id == ColorControl::Attributes::CurrentSaturation::Id) {
+                err = app_driver_light_set_saturation(handle, val);
+            } else if (attribute_id == ColorControl::Attributes::ColorTemperatureMireds::Id) {
+                err = app_driver_light_set_temperature(handle, val);
+            }
+        }
+    }
+    return err;
+}
+
+esp_err_t app_driver_light_set_defaults(uint16_t endpoint_id)
+{
+    esp_err_t err = ESP_OK;
+    void *priv_data = endpoint::get_priv_data(endpoint_id);
+    led_indicator_handle_t handle = (led_indicator_handle_t)priv_data;
+    esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+
+    /* Setting brightness */
+    attribute_t *attribute = attribute::get(endpoint_id, LevelControl::Id, LevelControl::Attributes::CurrentLevel::Id);
+    attribute::get_val(attribute, &val);
+    err |= app_driver_light_set_brightness(handle, &val);
+
+    /* Setting color */
+    attribute = attribute::get(endpoint_id, ColorControl::Id, ColorControl::Attributes::ColorMode::Id);
+    attribute::get_val(attribute, &val);
+    if (val.val.u8 == (uint8_t)ColorControl::ColorMode::kCurrentHueAndCurrentSaturation) {
+        /* Setting hue */
+        attribute = attribute::get(endpoint_id, ColorControl::Id, ColorControl::Attributes::CurrentHue::Id);
+        attribute::get_val(attribute, &val);
+        err |= app_driver_light_set_hue(handle, &val);
+        /* Setting saturation */
+        attribute = attribute::get(endpoint_id, ColorControl::Id, ColorControl::Attributes::CurrentSaturation::Id);
+        attribute::get_val(attribute, &val);
+        err |= app_driver_light_set_saturation(handle, &val);
+    } else if (val.val.u8 == (uint8_t)ColorControl::ColorMode::kColorTemperature) {
+        /* Setting temperature */
+        attribute = attribute::get(endpoint_id, ColorControl::Id, ColorControl::Attributes::ColorTemperatureMireds::Id);
+        attribute::get_val(attribute, &val);
+        err |= app_driver_light_set_temperature(handle, &val);
+    } else {
+        ESP_LOGE(TAG, "Color mode not supported");
+    }
+
+    /* Setting power */
+    attribute = attribute::get(endpoint_id, OnOff::Id, OnOff::Attributes::OnOff::Id);
+    attribute::get_val(attribute, &val);
+    err |= app_driver_light_set_power(handle, &val);
+
+    return err;
+}
+
+app_driver_handle_t app_driver_light_init()
+{
+#if CONFIG_BSP_LEDS_NUM > 0
+    /* Initialize led */
+    led_indicator_handle_t leds[CONFIG_BSP_LEDS_NUM];
+    ESP_ERROR_CHECK(bsp_led_indicator_create(leds, NULL, CONFIG_BSP_LEDS_NUM));
+    led_indicator_set_hsv(leds[0], SET_HSV(DEFAULT_HUE, DEFAULT_SATURATION, DEFAULT_BRIGHTNESS));
+    
+    return (app_driver_handle_t)leds[0];
+#else
+    return NULL;
+#endif
+}
+
+app_driver_handle_t app_driver_button_init()
+{
+    /* Initialize button */
+    button_handle_t btns[BSP_BUTTON_NUM];
+    ESP_ERROR_CHECK(bsp_iot_button_create(btns, NULL, BSP_BUTTON_NUM));
+    ESP_ERROR_CHECK(iot_button_register_cb(btns[0], BUTTON_PRESS_DOWN, app_driver_button_toggle_cb, NULL));
+    
+    return (app_driver_handle_t)btns[0];
+}
+
+int app_driver_motor_init()
+{
+    /* Initialize motor */
+    ESP_LOGI(TAG, "Create timer and operator");
+    mcpwm_timer_config_t timer_config = {
+        .group_id = 0,
+        .clk_src = MCPWM_TIMER_CLK_SRC_DEFAULT,
+        .resolution_hz = SERVO_TIMEBASE_RESOLUTION_HZ,
+        .count_mode = MCPWM_TIMER_COUNT_MODE_UP,
+        .period_ticks = SERVO_TIMEBASE_PERIOD,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_timer(&timer_config, &timer));
+
+    mcpwm_operator_config_t operator_config = {
+        .group_id = 0,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_operator(&operator_config, &oper));
+
+    ESP_LOGI(TAG, "Connect timer and operator");
+    ESP_ERROR_CHECK(mcpwm_operator_connect_timer(oper, timer));
+
+    ESP_LOGI(TAG, "Create comparator and generator from the operator");
+    mcpwm_comparator_config_t comparator_config;
+    comparator_config.flags.update_cmp_on_tez = true;
+    comparator_config.intr_priority = 0;
+
+    ESP_ERROR_CHECK(mcpwm_new_comparator(oper, &comparator_config, &comparator));
+
+    mcpwm_generator_config_t generator_config = {
+        .gen_gpio_num = SERVO_PULSE_GPIO,
+    };
+    ESP_ERROR_CHECK(mcpwm_new_generator(oper, &generator_config, &generator));
+
+    // Set the initial compare value to stop the servo
+    ESP_ERROR_CHECK(mcpwm_comparator_set_compare_value(comparator, SERVO_STOP_PULSEWIDTH_US));
+
+    ESP_LOGI(TAG, "Set generator action on timer and compare event");
+    // Go high on counter empty
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_timer_event(generator,
+                                                              MCPWM_GEN_TIMER_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, MCPWM_TIMER_EVENT_EMPTY, MCPWM_GEN_ACTION_HIGH)));
+    // Go low on compare threshold
+    ESP_ERROR_CHECK(mcpwm_generator_set_action_on_compare_event(generator,
+                                                                MCPWM_GEN_COMPARE_EVENT_ACTION(MCPWM_TIMER_DIRECTION_UP, comparator, MCPWM_GEN_ACTION_LOW)));
+
+    ESP_LOGI(TAG, "Motor driver initialized");
+
+    return 0;
+}
+
